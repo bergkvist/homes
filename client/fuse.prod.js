541669c5e50ca01be74fde0cde676d9fef4a86f0
Tobias Bergkvist <tobias@bergkv.ist>
2019-12-29 04:05:38 +0800

    Add target:browser to fuse.prod.js

--- client/fuse.prod.js
+++ client/fuse.prod.js
@@ -3,7 +3,8 @@ const { fusebox, pluginLink } = require('fuse-box')
 fusebox({
     webIndex: { template: './src/index.html' },
     entry: './src/index.ts',
+    target: 'browser',
     plugins: [
         pluginLink(/\.csv/, { useDefault: true })
     ],
-}).runProd()
+}).runProd({ uglify: true, target: 'ES6', screwIE: true })
