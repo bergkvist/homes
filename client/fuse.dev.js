23372aa206bf5c822fab3f7284d0402cdeac5cb1
Tobias Bergkvist <tobias@bergkv.ist>
2020-04-24 03:19:41 +0200

    New frontend client, written in React, bundled with parcel.

--- client/fuse.dev.js
+++ client/fuse.dev.js
@@ -1,11 +1,14 @@
 const { fusebox, pluginLink } = require('fuse-box')
 
 fusebox({
+    target: 'browser',
     webIndex: { template: './src/index.html' },
     entry: './src/index.ts',
     watch: true,
+    cache: { strategy: 'memory', enabled: true },
     devServer: { httpServer: { port: 8000 }, hmrServer: { port: 9999 } },
     plugins: [
         pluginLink(/\.csv/, { useDefault: true })
-    ]
+    ],
+    hmr: { enabled: true }
 }).runDev()
